bdec38c04d55bff05d69db420112aa89a9a95a8d
fsimoess <fsmendonca@inf.ufsm.br>
2018-04-22 21:33:21 -0300

    Update firesim1.cpp

--- trabalhos/t5/firesim1.cpp
+++ trabalhos/t5/firesim1.cpp
@@ -1,6 +1,6 @@
 //  
-// Simulação de incêndio em uma floresta.
-// Baseada no código proposto por David Joiner.
+// SimulaÃ§Ã£o de incÃªndio em uma floresta.
+// Baseada no cÃ³digo proposto por David Joiner.
 //
 // Uso: firesim <tamanho-do-problema> <nro. experimentos> <probab. maxima> 
 
@@ -14,7 +14,7 @@
 #include <omp.h>
 
 void
-checkCommandLine(int argc, char** argv, int& size, int& trials, int& probs)
+checkCommandLine(int argc, char** argv, int& size, int& trials, int& probs, int& threads)
 {
    if (argc > 1) {
       size = atoi(argv[1]);
@@ -24,19 +24,23 @@ checkCommandLine(int argc, char** argv, int& size, int& trials, int& probs)
    }
    if (argc > 3) {
       probs = atoi(argv[3]);
-   }   
+   }
+   if (argc > 4) {
+      threads = atoi(argv[4]);
+   }    
 }
 
 int 
 main(int argc, char* argv[]) 
 {
    
-   // parâmetros dos experimentos
+   // parÃ¢metros dos experimentos
    int forest_size = 30;
    int n_trials = 5000;
    int n_probs = 101;
+   int n_threads = 2;
 
-   double* percent_burned; // percentuais queimados (saída)
+   double* percent_burned; // percentuais queimados (saÃ­da)
    double* prob_spread;    // probabilidades (entrada)
    double prob_min = 0.0;
    double prob_max = 1.0;
@@ -44,11 +48,13 @@ main(int argc, char* argv[])
    int base_seed = 100;
    clock_t ini, fim;
 
-   checkCommandLine(argc, argv, forest_size, n_trials, n_probs);
+   checkCommandLine(argc, argv, forest_size, n_trials, n_probs, n_threads);
     
+   omp_set_num_threads(n_threads);
+
    try {
 		
-	  ini = clock();
+      ini = clock();
       Random rand;
       
       int ip, it;
@@ -60,8 +66,8 @@ main(int argc, char* argv[])
 
       printf("Probabilidade, Percentual Queimado\n");
 
-      // para cada probabilidade, calcula o percentual de árvores queimadas
-      //Opção 1: paralelizar calculo de percentual
+      // para cada probabilidade, calcula o percentual de Ã¡rvores queimadas
+      //OpÃ§Ã£o 1: paralelizar calculo de percentual
       #pragma omp parallel private(ip, it)
       {
       	Forest* forest = new Forest(forest_size);
@@ -69,26 +75,26 @@ main(int argc, char* argv[])
 	      for (ip = 0; ip < n_probs; ip++) {
 	      	 prob_spread[ip] = prob_min + (double) ip * prob_step;
 	         percent_burned[ip] = 0.0;
-	         rand.setSeed(base_seed+ip); // nova seqüência de números aleatórios
-	         // executa vários experimentos
+	         rand.setSeed(base_seed+ip); // nova seqÃ¼Ãªncia de nÃºmeros aleatÃ³rios
+	         // executa vÃ¡rios experimentos
 	         for (it = 0; it < n_trials; it++) {
-	            // queima floresta até o fogo apagar
+	            // queima floresta atÃ© o fogo apagar
 	            forest->burnUntilOut(forest->centralTree(), prob_spread[ip], rand);
 	            percent_burned[ip] += forest->getPercentBurned();
 	         }
 	
-	         // calcula média dos percentuais de árvores queimadas
+	         // calcula mÃ©dia dos percentuais de Ã¡rvores queimadas
 	         percent_burned[ip] /= n_trials;
 	
 	         // mostra resultado para esta probabilidade
-	         printf("%lf, %lf\n", prob_spread[ip], percent_burned[ip]);
+	         printf("Thread %d: %lf, %lf\n", omp_get_thread_num()+1, prob_spread[ip], percent_burned[ip]);
 	      }
 	  }
 
       delete[] prob_spread;
       delete[] percent_burned;
       fim = clock();
-      printf("Tempo de execuckao = %lf segundos\n", (double)(fim-ini)/CLOCKS_PER_SEC);
+      printf("Tempo de execucao = %lf segundos\n", (double)(fim-ini)/CLOCKS_PER_SEC);
    }
    catch (std::bad_alloc)
    {
